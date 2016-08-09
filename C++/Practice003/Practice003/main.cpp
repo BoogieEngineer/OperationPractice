70bfd05a211b7fa751c1fc058c110426e6cddf6b
Minh Ha <hahongminh@gmail.com>
2016-08-09 09:49:05 -0700

    Some code comments

--- C++/Practice003/Practice003/main.cpp
+++ C++/Practice003/Practice003/main.cpp
@@ -3,21 +3,29 @@
 
 using namespace std;
 
-class RGBA
+class RGBA // All class members default to be private
 {
-	int m_red;
-	int m_green;
-	int m_blue;
-	int m_alpha;
+	uint8_t m_red;
+	uint8_t m_green;
+	uint8_t m_blue;
+	uint8_t m_alpha;
 
 public:
+	// Constructor prototype, taking 4 arguments:
 	RGBA(int, int, int, int);
+	// Function to print:
 	void print()
 	{
-		cout << "Red = " << m_red << ", Green = " << m_green << ", Blue = " << m_blue << ", Alpha = " << m_alpha << "\n";
+		cout << "Red = " << static_cast<int>(m_red)
+			<< ", Green = " << static_cast<int>(m_green)
+			<< ", Blue = " << static_cast<int>(m_blue)
+			<< ", Alpha = " << static_cast<int>(m_alpha)
+			<< "\n";
 	}
 };
 
+// Constructor using member initialization list:
+// (if args not given at function call, the call will use the defaults in the constructor declaration)
 RGBA::RGBA(int r=0, int g=0, int b=0, int a=255):
     m_red(r),
 	m_green(g),
@@ -29,7 +37,9 @@ RGBA::RGBA(int r=0, int g=0, int b=0, int a=255):
 int main()
 {
 	RGBA teal(0, 127, 127);
+	RGBA crap(0, 44);
 	teal.print();
+	crap.print();
 
 	cin.get();
 	return 0;
