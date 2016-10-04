257a725905603a3ba65373147984fe22eb35c7e8
Minh Ha <hahongminh@gmail.com>
2016-10-04 13:23:07 -0700

    complete C++ course hw1

--- C++ Courses/Course I/Assignments/C1A1E2_main.cpp
+++ C++ Courses/Course I/Assignments/C1A1E2_main.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    int x; // define variable that will take value from user-input
+    cout << "Please provide an integer: "; //prompting the user to provide input
+    cin >> x; // read user input
+
+    cout << x << " decimal = " << oct << x << " octal = " << hex << x << " hexadecimal"; // print the results
+
+    cin.get();
+    cin.get();
+    return 0;
+}
