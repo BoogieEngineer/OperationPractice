736eb4af6ed462682895c3c1b39f6c1d77bf3795
Minh Ha <hahongminh@gmail.com>
2016-10-03 17:29:32 -0700

    Add assignments for C++ Course I

---
+++ C++ Courses/Course I/Assignments/C1A1E1_main.c
@@ -0,0 +1,29 @@
+/*
+* Student name: Minh Ha, UCSD student ID U07470714
+* Email address: hahongminh@gmail.com
+* Course: C/C++ Programming I Fundamental Programming Concepts, Section ID 118011, instructor: Raymond Mitchell.
+* Date: 10/03/2016.
+* Name of this file: C1A1E1_main.c
+* Operating system: Windows 7.
+* Compiler & version: Visual C++ 2010.
+*
+* Names of functions defined in the file with a general overview of what
+* they do; descriptions of anything else in the file you consider relevant.
+* PLEASE BE ORIGINAL - PLEASE DO NOT COPY OR RESTATE MY INSTRUCTIONS.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("In C/C++ the case of letters is significant.\n"
+        "main is where program execution begins.\n"
+        "A semicolon terminates most statements.\n"
+        "10%% of \"nothing\" is 100%% of \"nothing\".\n"
+        "Use \\n to cause a newline; use \\t to cause a tab.\n"
+        "Use \\a to cause a beep (only on some platforms)!");
+
+    getchar();
+    return(EXIT_SUCCESS);
+}
