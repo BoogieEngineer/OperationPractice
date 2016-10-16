11c7d8c018e11e0b5e3b2a1f44905a4065049d83
Ha Hong Minh <Ha Hong Minh>
2016-10-16 13:19:43 -0700

    add A3

---
+++ C++ Courses/Course I/Assignments/C1A3E1_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    //E0 answer: 5A
+    putchar('A') && putchar('B');
+
+    printf("Hi") && printf("World");
+
+    return(EXIT_SUCCESS);
+}
