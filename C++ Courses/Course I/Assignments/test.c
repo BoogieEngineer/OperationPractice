d4c2c3a197a0890a597981af5e92861fb822c804
Minh Ha <hahongminh@gmail.com>
2016-10-24 13:36:08 -0700

    fix A4

---
+++ C++ Courses/Course I/Assignments/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cstdlib>
+
+int main(void)
+{
+
+    printf("%d", int('A'));
+
+    return(EXIT_SUCCESS);
+}
---
+++ C++ Courses/Course I/Assignments/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << (int)('A');
+    return 0;
+}
