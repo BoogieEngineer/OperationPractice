b809e1f1b45ce37d23c79b0120f031637ceba373
Minh Ha <hahongminh@gmail.com>
2016-10-25 15:30:23 -0700

    finished A4

--- C++ Courses/Course I/Assignments/C1A4E4_MaxOf.h
+++ C++ Courses/Course I/Assignments/C1A4E4_MaxOf.h
@@ -7,4 +7,29 @@
  * Operating system: Windows 7.
  * Compiler & version: Visual C++ 2010.
  *
+ * This header file contains macros and inline functions that both determine the
+ * maximum value of any 3 numbers of type long double.
  */
+
+#ifndef C1A4E4_MAXOF_H // Include guard begins
+#define C1A4E4_MAXOF_H
+
+// Function-like macros to determine the maximum of any 3 numbers
+#define mMaxOf2(firstNumber, secondNumber) ((firstNumber) > (secondNumber) ? \
+    (firstNumber) : (secondNumber))
+#define mMaxOf3(firstNumber, secondNumber, thirdNumber) \
+    (mMaxOf2(mMaxOf2((firstNumber), (secondNumber)), (thirdNumber)))
+
+// Inline functions to determine the maximum of any 3 numbers
+inline long double fMaxOf2(long double firstNumber, long double secondNumber)
+{
+    return firstNumber > secondNumber ? firstNumber : secondNumber;
+}
+
+inline long double fMaxOf3(long double firstNumber, long double secondNumber,
+    long double thirdNumber)
+{
+    return fMaxOf2(fMaxOf2(firstNumber, secondNumber), thirdNumber);
+}
+
+#endif // Inlcude guard ends
--- C++ Courses/Course I/Assignments/C1A4E4_main.cpp
+++ C++ Courses/Course I/Assignments/C1A4E4_main.cpp
@@ -7,4 +7,28 @@
  * Operating system: Windows 7.
  * Compiler & version: Visual C++ 2010.
  *
+ * This program takes 3 numbers from user input and print out the maximum of them using
+ * the function-like macros and inline functions in the header file C1A4E4_MaxOf.h.
  */
+
+#include <iostream>
+#include "C1A4E4_MaxOf.h"
+
+using namespace std;
+
+int main()
+{
+    long double firstNumber, secondNumber, thirdNumber;
+
+    // Taking user input numbers:
+    cout << "Enter 3 space-separated decimal numeric values: ";
+    cin >> firstNumber >> secondNumber >> thirdNumber;
+
+    // Calling the macros and inline functions and print out the results:
+    cout << "mMaxOf3(" << firstNumber << ", " << secondNumber << ", " << thirdNumber
+        << ") returned " << mMaxOf3(firstNumber, secondNumber, thirdNumber) << "\n";
+    cout << "fMaxOf3(" << firstNumber << ", " << secondNumber << ", " << thirdNumber
+        << ") returned " << fMaxOf3(firstNumber, secondNumber, thirdNumber) << "\n";
+
+    return 0;
+}
