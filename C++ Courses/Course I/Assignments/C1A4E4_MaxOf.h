60e2ce39654d2d2958a4a3472ed5aa5ba822258a
Minh Ha <hahongminh@gmail.com>
2016-10-25 13:17:27 -0700

    doing A4

---
+++ C++ Courses/Course I/Assignments/C1A4E4_MaxOf.h
@@ -0,0 +1,10 @@
+/*
+ * Student name: Minh Ha, UCSD student ID U07470714
+ * Email address: hahongminh@gmail.com
+ * Course: C/C++ Programming I, Section ID 118011, instructor: Raymond Mitchell.
+ * Date: 10/25/2016.
+ * Name of this file: C1A4E4_MaxOf.h
+ * Operating system: Windows 7.
+ * Compiler & version: Visual C++ 2010.
+ *
+ */
