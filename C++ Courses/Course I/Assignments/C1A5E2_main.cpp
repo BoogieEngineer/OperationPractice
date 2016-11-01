29b4f9b0dd260853a3ec0db31042af3cc85e7ad1
Ha Hong Minh <Ha Hong Minh>
2016-10-31 20:01:54 -0700

    add C1A5E2

---
+++ C++ Courses/Course I/Assignments/C1A5E2_main.cpp
@@ -0,0 +1,21 @@
+/*
+* Student name: Minh Ha, UCSD student ID U07470714
+* Email address: hahongminh@gmail.com
+* Course: C/C++ Programming I, Section ID 118011, instructor: Raymond Mitchell.
+* Date: 10/31/2016.
+* Name of this file: C1A5E2_main.cpp
+* Operating system: Windows 7.
+* Compiler & version: Visual C++ 2010.
+*
+* 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
