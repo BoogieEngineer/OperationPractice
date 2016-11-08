e0c8e32107dd2b787418b80e4afbabc87f217d6d
Ha Hong Minh <Ha Hong Minh>
2016-11-08 00:26:54 -0800

    Fixing A6

--- C++ Courses/Course I/Assignments/C1A6E2_MyStrcmp.c
+++ C++ Courses/Course I/Assignments/C1A6E2_MyStrcmp.c
@@ -7,7 +7,8 @@
  * Operating system: Windows 7.
  * Compiler & version: Visual C++ 2010.
  *
- * This function compares 2 strings.
+ * This function compares 2 strings by stepping through them one char
+ * at a time to spot the first unequal pair of characters.
  */
 
 int MyStrcmp(const char *s1, const char *s2)
@@ -16,10 +17,13 @@ int MyStrcmp(const char *s1, const char *s2)
     // find the first unequal corresponding characters.
     while ((*s1) || (*s2))
     {
-        // If 2 corresponding chars are unequal, return the difference between them
+        /* If 2 corresponding chars are unequal, return the difference between them
+         * This also applies for cases where one of the two chars being compared
+         * is the null terminator.
+         */
         if (*s1 != *s2)
         {
-            return(*s1 - *s2);
+            return(int)(*s1 - *s2);
         }
         else if (*s1 == '\0' && *s2 != '\0')
             return(-*s2);
