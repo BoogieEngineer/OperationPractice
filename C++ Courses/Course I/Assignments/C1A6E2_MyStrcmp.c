de67e1222727ff2100b0dce65230d4c07e2eece6
Ha Hong Minh <Ha Hong Minh>
2016-11-08 00:31:10 -0800

    Fixing A6

--- C++ Courses/Course I/Assignments/C1A6E2_MyStrcmp.c
+++ C++ Courses/Course I/Assignments/C1A6E2_MyStrcmp.c
@@ -23,7 +23,7 @@ int MyStrcmp(const char *s1, const char *s2)
          */
         if (*s1 != *s2)
         {
-            return(int)(*s1 - *s2);
+            return (int)(*s1 - *s2);
         }
         else if (*s1 == '\0' && *s2 != '\0')
             return(-*s2);
