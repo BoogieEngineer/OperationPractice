e4c2087b80764c6bd88197a9043d7b656a8716a5
Ha Hong Minh <Ha Hong Minh>
2016-11-15 20:24:49 -0800

    Finish A7

--- C++ Courses/Course I/Assignments/C1A7E2_main.c
+++ C++ Courses/Course I/Assignments/C1A7E2_main.c
@@ -17,7 +17,8 @@
 
 // Define some necessary macros
 #define LUNCH_ITEMS 3
-#define FOOD_NAME_LENGTH 128
+#define MINIMUM_ITEMS 2
+#define FOOD_NAME_LENGTH 129
 #define Elements(A) (sizeof(A)/sizeof(*(A)))
 
 int main(void)
@@ -27,28 +28,27 @@ int main(void)
     {
         char *name;
         int weight, calories;
-    } lunch[LUNCH_ITEMS] = { { "Apple", 4, 100 }, { "Salad", 2, 80 } };
+    } lunch[LUNCH_ITEMS] = {{"Apple", 4, 100}, {"Salad", 2, 80}};
 
     // Loop to get user information for the uninitialized foods
     int iteration;
-    for (iteration = 2; iteration < LUNCH_ITEMS; iteration++)
+    for (iteration = MINIMUM_ITEMS; iteration < LUNCH_ITEMS; iteration++)
     {
         // Create a buffer for food name
         char foodName[FOOD_NAME_LENGTH];
 
         printf("Enter food name, weight and calories: ");
         //fgets(foodName, FOOD_NAME_LENGTH, stdin);
-        scanf("%s %i %i", foodName, &lunch[iteration].weight, &lunch[iteration].calories);
+        scanf("%128s %i %i", foodName, &lunch[iteration].weight, &lunch[iteration].calories);
         
         // Check if malloc is successful
-        if ((lunch[iteration].name = (char *)malloc(Elements(foodName) * sizeof(char))) == NULL)
+        if ((lunch[iteration].name = (char *)malloc(Elements(foodName))) == NULL)
         {
             fputs("Out of memory\n", stderr);
             exit(EXIT_FAILURE);
         }
         // Copy user input string to food name using memcpy
-        memcpy(lunch[iteration].name, foodName, sizeof(foodName));
-        //printf("%i %i %s", (int*)strlen(foodName), strlen(lunch[iteration].name), lunch[iteration].name);
+        memcpy(lunch[iteration].name, foodName, strlen(foodName) + 1);
     }
 
     // Print the result table
@@ -56,5 +56,9 @@ int main(void)
     {
         printf("%-12s %7i %7i\n", lunch[iteration].name, lunch[iteration].weight, lunch[iteration].calories);
     }
+
+    // Free any dynamically allocated memory
+    for (iteration = MINIMUM_ITEMS; iteration < LUNCH_ITEMS; iteration++)
+        free(lunch[iteration].name);
     return(EXIT_SUCCESS);
 }
