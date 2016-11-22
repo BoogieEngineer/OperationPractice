71963660e32cb1018a0418f3d4e0b1c1d13d9dbf
Minh Ha <hahongminh@gmail.com>
2016-11-21 17:31:27 -0800

    add nothing

--- C++ Courses/Course I/Assignments/test.cpp
+++ C++ Courses/Course I/Assignments/test.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 #include <string>
+#include <bitset>
 
 using namespace std;
 
-struct junk
-{
-    int a;
-};
-
-void fcn(struct junk *);
-
 int main()
 {
-
-
-    fcn(&z)
-
+    int a = 7;
+    long b = 1 << a;
+    cout << bitset<8>(b) << "\n" << bitset<8>(128);
     return 0;
 }
