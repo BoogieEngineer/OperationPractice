ad62a7c0201ca8603eaa4214e75fcbc7c7e95a4a
Minh Ha <hahongminh@gmail.com>
2016-11-22 18:21:42 -0800

    doing A8

--- C++ Courses/Course I/Assignments/C1A8E2_main.c
+++ C++ Courses/Course I/Assignments/C1A8E2_main.c
@@ -14,18 +14,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LAST_COMMAND_LINE_INPUT_RIGHT_INDEX 1
+#define SECOND_LAST_COMMAND_LINE_INPUT_RIGHT_INDEX 2
+
 int main(int argc, char *argv[])
 {
-    int numberOfLines = atoi(argv[2]);
-    FILE *textFile = fopen(argv[1], "r");
+    // Declare necessary variables
+    char *fileName = argv[argc - SECOND_LAST_COMMAND_LINE_INPUT_RIGHT_INDEX];
+    int lineGroupLength = atoi(argv[argc - LAST_COMMAND_LINE_INPUT_RIGHT_INDEX]),
+        inputValue, lineCount = 0;
 
-    if (textFile == NULL)
+    // Open the file
+    FILE *file = fopen(fileName, "r");
+
+    // Check if the open file action is successful
+    if (file == NULL)
     {
         fprintf(stderr, "Cannot open the required file\n");
         exit(EXIT_FAILURE);
     }
 
-    fclose(textFile);
+    // Print one char at a time until getting EOF
+    while ((inputValue = getc(file)) != EOF)
+    {
+        printf("%c", inputValue);
+        // If encountering newline, increase the line count. Once line count reaches
+        // the required line group length, stop and wait for user to press Enter
+        if (inputValue == '\n')
+        {    
+            lineCount++;
+            if (lineCount == lineGroupLength)
+            {
+                // If user presses anything rather than Enter, break
+                if (getchar() != '\n')
+                    break;
+                lineCount = 0;
+            }
+        }
+    }
+
+    fclose(file);
 
     return(EXIT_SUCCESS);
 }
